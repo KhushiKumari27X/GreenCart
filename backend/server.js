912a4e94b5c89dddaa8d7c4ea7ea5624921a5fce
KhushiKumari27X <khushikumarikh27@gmail.com>
2026-05-11 18:30:12 +0530

    Testing minimal server

--- backend/server.js
+++ backend/server.js
@@ -225,50 +225,70 @@
 // // ==========================================
 // export default app;
 
+// import express from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import "dotenv/config";
+
+// import connectDB from "./configs/db.js";
+// import connectCloudinary from "./configs/cloudinary.js";
+
+// import userRouter from "./routes/userRoute.js";
+// import sellerRouter from "./routes/sellerRoute.js";
+// import productRouter from "./routes/productRoute.js";
+// import cartRouter from "./routes/cartRoute.js";
+// import addressRouter from "./routes/addressRoute.js";
+// import orderRouter from "./routes/orderRoute.js";
+
+// const app = express();
+
+// // DATABASE
+// connectDB();
+
+// // CLOUDINARY
+// connectCloudinary();
+
+// // CORS
+// app.use(cors({
+//   origin: "https://green-cart-mocha-phi.vercel.app",
+//   credentials: true
+// }));
+
+// // MIDDLEWARES
+// app.use(express.json());
+// app.use(cookieParser());
+
+// // TEST ROUTE
+// app.get("/", (req, res) => {
+//   res.send("API Working");
+// });
+
+// // ROUTES
+// app.use("/api/user", userRouter);
+// app.use("/api/seller", sellerRouter);
+// app.use("/api/product", productRouter);
+// app.use("/api/cart", cartRouter);
+// app.use("/api/address", addressRouter);
+// app.use("/api/order", orderRouter);
+
+// export default app;
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import "dotenv/config";
-
-import connectDB from "./configs/db.js";
-import connectCloudinary from "./configs/cloudinary.js";
-
-import userRouter from "./routes/userRoute.js";
-import sellerRouter from "./routes/sellerRoute.js";
-import productRouter from "./routes/productRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import addressRouter from "./routes/addressRoute.js";
-import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 
-// DATABASE
-connectDB();
-
-// CLOUDINARY
-connectCloudinary();
-
-// CORS
 app.use(cors({
   origin: "https://green-cart-mocha-phi.vercel.app",
   credentials: true
 }));
 
-// MIDDLEWARES
 app.use(express.json());
 app.use(cookieParser());
 
-// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("API Working");
 });
-
-// ROUTES
-app.use("/api/user", userRouter);
-app.use("/api/seller", sellerRouter);
-app.use("/api/product", productRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/address", addressRouter);
-app.use("/api/order", orderRouter);
 
 export default app;
