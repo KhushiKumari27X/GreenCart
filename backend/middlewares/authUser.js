51c001b7bc576a23bd6c45cbcdbe704be488f772
KhushiKumari27X <khushikumarikh27@gmail.com>
2026-05-16 19:17:07 +0530

    final deployment setup

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -1,47 +1,4 @@
 
-// import jwt from "jsonwebtoken";
-
-// const authUser = async (req, res, next) => {
-
-//     try {
-
-//         const token = req.cookies.token;
-
-//         // CHECK TOKEN
-//         if (!token) {
-
-//             return res.json({
-//                 success: false,
-//                 message: "Not Authorized"
-//             });
-
-//         }
-
-//         // VERIFY TOKEN
-//         const decoded = jwt.verify(
-//             token,
-//             process.env.JWT_SECRET
-//         );
-
-//         // SAVE USER ID
-//         req.userId = decoded.id;
-
-//         next();
-
-//     } catch (error) {
-
-//         console.log(error.message);
-
-//         return res.json({
-//             success: false,
-//             message: error.message
-//         });
-
-//     }
-
-// };
-
-// export default authUser;
 import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
